c76ed2600ee0f654f05e30d577268de1a7e7a73b
MostafizurRahman199 <fardilshifat199@gmail.com>
2025-07-11 15:58:21 +0600

    some practice question

--- practice.sql
+++ practice.sql
@@ -64,8 +64,70 @@ JOIN departments USING(department_id)
 GROUP BY department_name
 
 
+select department_name, count(*) from employees
+JOIN departments USING(department_id)
+GROUP BY department_name;
 
 
 
 
+SELECT department_name
+FROM employees
+JOIN departments USING (department_id)
+GROUP BY department_name
+ORDER BY AVG(salary) DESC
+LIMIT 1;
 
+
+select department_name, round(avg(salary)) from employees
+join departments using(department_id)
+group by department_name
+order by avg(salary) desc limit 1;
+
+
+
+
+SELECT EXTRACT(YEAR FROM hire_date) AS hire_year, COUNT(*) AS total_hired
+FROM employees
+GROUP BY hire_year
+ORDER BY hire_year;
+
+
+CREATE TABLE orders (
+    order_id SERIAL PRIMARY KEY,
+    customer_id INT NOT NULL,
+    order_date DATE NOT NULL,
+    total_amount DECIMAL(10, 2) NOT NULL
+);
+
+
+
+INSERT INTO orders (customer_id, order_date, total_amount)
+VALUES 
+(201, '2025-07-11', 500.00),
+(202, '2025-07-10', 150.75),
+(203, '2025-07-09', 340.20),
+(204, '2025-07-08', 99.99),
+(205, '2025-07-07', 199.00),
+(205, '2024-07-07', 199.00),
+(205, '2025-07-07', 199.00),
+(205, '2025-07-07', 199.00),
+(205, '2025-07-07', 199.00),
+(205, '2025-07-07', 199.00);
+
+
+SELECT customer_id, COUNT(*) AS total_orders, SUM(total_amount) AS total_spent
+FROM orders
+GROUP BY customer_id
+HAVING COUNT(*) > 2;
+
+
+
+SELECT 
+  TO_CHAR(order_date, 'Month') AS month_name,
+  DATE_TRUNC('month', order_date)::DATE AS month_start,
+  COUNT(*) AS total_orders
+FROM orders
+WHERE EXTRACT(YEAR FROM order_date) = 2024
+GROUP BY month_name, month_start
+ORDER BY month_start;
